55cd792be94f3f78c7ae5296ca2ae7fa68276b11
FatalMerlin <merlin.brandes@gmail.com>
2025-04-03 00:35:59 +0200

    fix: VehiclesRentalsPricesAllEntity PriceRent type

--- ArkanisOverlay/Data/Entities/UEX/VehiclesRentalsPricesAllEntity.cs
+++ ArkanisOverlay/Data/Entities/UEX/VehiclesRentalsPricesAllEntity.cs
@@ -6,7 +6,7 @@ public class VehiclesRentalsPricesAllEntity : BaseEntity
 {
     [JsonPropertyName("id_vehicle")] public int IdVehicle { get; set; }
     [JsonPropertyName("id_terminal")] public int IdTerminal { get; set; }
-    [JsonPropertyName("price_rent")] public float PriceRent { get; set; } // last
+    [JsonPropertyName("price_rent")] public decimal PriceRent { get; set; } // last
     [JsonPropertyName("vehicle_name")] public string VehicleName { get; set; }
     [JsonPropertyName("terminal_name")] public string TerminalName { get; set; }
 }
---
+++ ArkanisOverlay/Migrations/20250402222244_Fix VehilcesRentalsPricesAll PriceRent Type.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArkanisOverlay.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixVehilcesRentalsPricesAllPriceRentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceRent",
+                table: "VehiclesRentalsPricesAll",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "REAL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "PriceRent",
+                table: "VehiclesRentalsPricesAll",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+    }
+}
