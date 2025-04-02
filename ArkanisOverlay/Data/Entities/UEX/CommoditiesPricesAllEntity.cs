e5b1c30a594922d981c4485627f62963bf762454
FatalMerlin <merlin.brandes@gmail.com>
2025-04-03 00:04:26 +0200

    fix: CommoditiesPricesAllEntity overwriting date added & modified

--- ArkanisOverlay/Data/Entities/UEX/CommoditiesPricesAllEntity.cs
+++ ArkanisOverlay/Data/Entities/UEX/CommoditiesPricesAllEntity.cs
@@ -18,8 +18,6 @@ public class CommoditiesPricesAllEntity : BaseEntity
     [JsonPropertyName("scu_sell_avg")] public decimal ScuSellAvg { get; set; }
     [JsonPropertyName("status_buy")] public int? StatusBuy { get; set; }
     [JsonPropertyName("status_sell")] public int? StatusSell { get; set; }
-    [JsonPropertyName("date_added")] public int DateAdded { get; set; } // timestamp
-    [JsonPropertyName("date_modified")] public int DateModified { get; set; } // timestamp
     [JsonPropertyName("commodity_name")] public string? CommodityName { get; set; }
     [JsonPropertyName("commodity_code")] public string? CommodityCode { get; set; }
     [JsonPropertyName("commodity_slug")] public string? CommoditySlug { get; set; }
