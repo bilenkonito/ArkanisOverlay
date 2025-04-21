3756bb236511628275af317a30bc9ec542210277
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-04-21 08:02:18 +0200

    fix(domain): add missing vehicle model dependency

--- src/Arkanis.Overlay.Infrastructure/Repositories/Sync/UexVehicleRepository.cs
+++ src/Arkanis.Overlay.Infrastructure/Repositories/Sync/UexVehicleRepository.cs
@@ -1,18 +1,24 @@
 namespace Arkanis.Overlay.Infrastructure.Repositories.Sync;
 
 using Data.Mappers;
+using Domain.Abstractions;
 using Domain.Models.Game;
 using External.UEX.Abstractions;
 using Local;
 using Microsoft.Extensions.Logging;
+using Services;
 
 internal class UexVehicleRepository(
+    GameEntityRepositoryDependencyResolver dependencyResolver,
     IUexGameApi gameApi,
     UexGameDataStateProvider stateProvider,
     UexApiDtoMapper mapper,
     ILogger<UexVehicleRepository> logger
 ) : UexGameEntityRepositoryBase<VehicleDTO, GameVehicle>(stateProvider, mapper, logger)
 {
+    protected override IDependable GetDependencies()
+        => dependencyResolver.DependsOn<GameCompany>();
+
     protected override async Task<UexApiResponse<ICollection<VehicleDTO>>> GetInternalResponseAsync(CancellationToken cancellationToken)
     {
         var response = await gameApi.GetVehiclesAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
