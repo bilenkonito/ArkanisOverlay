c13e82e16156f954dca0fd88e3fb7ab447dea55a
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-04-24 08:22:37 +0200

    docs: mention issues with deadlocking

--- src/Arkanis.Overlay.Infrastructure/Services/Abstractions/IHydrationServiceFor.cs
+++ src/Arkanis.Overlay.Infrastructure/Services/Abstractions/IHydrationServiceFor.cs
@@ -2,6 +2,9 @@ namespace Arkanis.Overlay.Infrastructure.Services.Abstractions;
 
 using Domain.Abstractions;
 
+/// <summary>
+///     A generic interface for hydration services that act as they can hydrate objects of any type.
+/// </summary>
 public interface IHydrationServiceFor
 {
     bool Matches<T>(T entity);
@@ -9,6 +12,13 @@ public interface IHydrationServiceFor
     Task HydrateAsync<T>(T entity, CancellationToken cancellationToken = default);
 }
 
+/// <summary>
+///     An interface for hydration services that can hydrate objects of a specific type.
+/// </summary>
+/// <remarks>
+///     Creating cyclic hydration dependencies will result in startup deadlock.
+/// </remarks>
+/// <typeparam name="T">Targeted object type</typeparam>
 public interface IHydrationServiceFor<in T> : IHydrationServiceFor, IDependable
 {
     bool IHydrationServiceFor.Matches<TEntity>(TEntity entity)
