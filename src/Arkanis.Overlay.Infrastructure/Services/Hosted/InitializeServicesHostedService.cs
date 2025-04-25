928b08cba21e68f1d8b5f4e6c4de39795c7d5465
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-04-25 19:02:40 +0200

    feat: add support for scheduled repository refreshing

--- src/Arkanis.Overlay.Infrastructure/Services/Hosted/InitializeServicesHostedService.cs
+++ src/Arkanis.Overlay.Infrastructure/Services/Hosted/InitializeServicesHostedService.cs
@@ -1,14 +1,20 @@
 namespace Arkanis.Overlay.Infrastructure.Services.Hosted;
 
 using Abstractions;
+using Domain.Models.Game;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Quartz;
 
 internal class InitializeServicesHostedService(
+    ISchedulerFactory schedulerFactory,
     IEnumerable<ISelfInitializable> services,
     ILogger<InitializeServicesHostedService> logger
 ) : BackgroundService
 {
+    private static readonly TimeSpan RefreshJobInterval = TimeSpan.FromMinutes(10);
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogDebug("Running initialization of services: {@Services}", services);
@@ -22,5 +28,27 @@ internal class InitializeServicesHostedService(
 
         await Task.WhenAll(tasks).ConfigureAwait(false);
         logger.LogDebug("Finished initialization of {ServiceCount} services", services.Count());
+
+        var scheduler = await schedulerFactory.GetScheduler(stoppingToken);
+
+        var genericJobType = typeof(GameEntityRepositorySyncManagerUpdateIfNecessaryJob<>);
+        foreach (var gameEntityType in GameEntityConstants.GameEntityTypes)
+        {
+            var jobType = genericJobType.MakeGenericType(gameEntityType);
+            var jobDetail = JobBuilder.Create(jobType)
+                .WithIdentity(jobType.ShortDisplayName())
+                .Build();
+            var trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(schedule => schedule
+                    .WithInterval(RefreshJobInterval)
+                    .WithMisfireHandlingInstructionIgnoreMisfires()
+                    .RepeatForever()
+                )
+                .StartAt(DateTimeOffset.UtcNow + RefreshJobInterval)
+                .ForJob(jobDetail)
+                .Build();
+
+            await scheduler.ScheduleJob(jobDetail, trigger, stoppingToken);
+        }
     }
 }
--- src/Arkanis.Overlay.Infrastructure/Services/Hosted/UpdateRepositoryIfNecessaryJob.cs
+++ src/Arkanis.Overlay.Infrastructure/Services/Hosted/UpdateRepositoryIfNecessaryJob.cs
@@ -1,11 +1,18 @@
 namespace Arkanis.Overlay.Infrastructure.Services.Hosted;
 
 using Domain.Abstractions.Game;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
 using Quartz;
 
-internal class GameEntityRepositoryUpdateIfNecessaryJob<T>(GameEntityRepositorySyncManager<T> repositorySyncManager)
-    : IJob where T : class, IGameEntity
+internal class GameEntityRepositorySyncManagerUpdateIfNecessaryJob<T>(
+    GameEntityRepositorySyncManager<T> repositorySyncManager,
+    ILogger<GameEntityRepositorySyncManagerUpdateIfNecessaryJob<T>> logger
+) : IJob where T : class, IGameEntity
 {
     public async Task Execute(IJobExecutionContext context)
-        => await repositorySyncManager.UpdateIfNecessaryAsync(context.CancellationToken);
+    {
+        logger.LogDebug("Running scheduled update (if necessary) for {GameEntityType}", typeof(T).ShortDisplayName());
+        await repositorySyncManager.UpdateIfNecessaryAsync(context.CancellationToken);
+    }
 }
