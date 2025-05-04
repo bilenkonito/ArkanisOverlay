6109a45805f3fa9bb9c1a52130c26764b86e09de
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-05-04 11:49:39 +0200

    fix: add environment information to tracked analytic events

--- src/Arkanis.Overlay.Components/Services/GoogleAnalyticsTracker.cs
+++ src/Arkanis.Overlay.Components/Services/GoogleAnalyticsTracker.cs
@@ -3,35 +3,52 @@ namespace Arkanis.Overlay.Components.Services;
 using Blazor.Analytics;
 using Domain.Abstractions.Services;
 using Domain.Models.Analytics;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-public class GoogleAnalyticsTracker(IAnalytics analytics, ILogger<GoogleAnalyticsTracker> logger) : IAnalyticsTracker
+public class GoogleAnalyticsTracker(IAnalytics analytics, IHostEnvironment hostEnvironment, ILogger<GoogleAnalyticsTracker> logger) : IAnalyticsTracker
 {
+    private const string EventCategoryKey = "event_category";
+    private const string EventLabelKey = "event_label";
+    private const string EnvironmentTypeKey = "environment_type";
+    private const string TrafficTypeKey = "traffic_type";
+
     public async Task TrackEventAsync(AnalyticsEvent analyticsEvent)
     {
         logger.LogDebug("Reporting event: {AnalyticsEventName}", analyticsEvent.EventName);
         await analytics.TrackEvent(analyticsEvent.EventName, CreateEventData(analyticsEvent));
     }
 
-    private static object CreateEventData(AnalyticsEvent analyticsEvent)
-        => analyticsEvent switch
+    private Dictionary<string, object> CreateEventData(AnalyticsEvent analyticsEvent)
+    {
+        var eventData = analyticsEvent switch
         {
             DialogOpenedEvent @event => CreateSpecificEventData(@event),
             EntitySearchEvent @event => CreateSpecificEventData(@event),
-            _ => new object(),
+            _ => [],
         };
 
-    private static object CreateSpecificEventData(EntitySearchEvent @event)
-        => new
+        AddCommonEventData(eventData);
+        return eventData;
+    }
+
+    private void AddCommonEventData(Dictionary<string, object> eventData)
+    {
+        eventData[EnvironmentTypeKey] = hostEnvironment.EnvironmentName;
+        eventData[TrafficTypeKey] = hostEnvironment.IsProduction() ? "public" : "internal";
+    }
+
+    private static Dictionary<string, object> CreateSpecificEventData(EntitySearchEvent @event)
+        => new()
         {
-            search_term = @event.Query,
+            ["search_term"] = @event.Query,
         };
 
-    private static object CreateSpecificEventData(DialogOpenedEvent @event)
-        => new
+    private static Dictionary<string, object> CreateSpecificEventData(DialogOpenedEvent @event)
+        => new()
         {
-            eventCategory = "Dialog",
-            eventAction = "Opened",
-            eventLabel = @event.DialogId,
+            [EventCategoryKey] = "Dialog",
+            [EventLabelKey] = "Opened",
+            ["value"] = @event.DialogId,
         };
 }
