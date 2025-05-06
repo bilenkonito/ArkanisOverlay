92da39d98b164990968b4606723cf9b9a339b9cc
FatalMerlin <merlin.brandes@gmail.com>
2025-05-06 09:28:14 +0200

    docs(native): add missing return documentation for GlobalHotkey HookProc

--- src/Arkanis.Overlay.Application/Workers/GlobalHotkey.cs
+++ src/Arkanis.Overlay.Application/Workers/GlobalHotkey.cs
@@ -5,7 +5,8 @@ using Windows.Win32.Foundation;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
 using Windows.Win32.UI.WindowsAndMessaging;
 using Domain.Abstractions.Services;
-using Helpers;using Microsoft.Extensions.Hosting;
+using Helpers;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PInvoke = Windows.Win32.PInvoke;
 
@@ -116,7 +117,13 @@ public sealed class GlobalHotkey : IHostedService, IDisposable
     ///     This parameter can be one of the following messages: WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, or WM_SYSKEYUP.
     /// </param>
     /// <param name="lparam">A pointer to a KBDLLHOOKSTRUCT structure.</param>
-    /// <returns></returns>
+    /// <returns>
+    ///     If nCode is less than zero, the hook procedure must return the value returned by CallNextHookEx.
+    ///
+    ///     If nCode is greater than or equal to zero, and the hook procedure did not process the message, it is highly recommended that you call CallNextHookEx and return the value it returns; otherwise, other applications that have installed WH_KEYBOARD_LL hooks will not receive hook notifications and may behave incorrectly as a result.
+    ///
+    ///     If the hook procedure processed the message, it may return a nonzero value to prevent the system from passing the message to the rest of the hook chain or the target window procedure.
+    /// </returns>
     private LRESULT HookProc(int nCode, WPARAM wparam, LPARAM lparam)
     {
         if (nCode < 0)
