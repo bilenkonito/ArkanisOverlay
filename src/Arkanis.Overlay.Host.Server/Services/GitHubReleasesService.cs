be067d32015db588d2886853cf1a69fb2a9bbe0d
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-05-09 21:11:09 +0200

    feat: add GitHub release grabbing - minor ui improvements

--- src/Arkanis.Overlay.Host.Server/Services/GitHubReleasesService.cs
+++ src/Arkanis.Overlay.Host.Server/Services/GitHubReleasesService.cs
@@ -2,6 +2,7 @@ namespace Arkanis.Overlay.Host.Server.Services;
 
 using Common;
 using Microsoft.Extensions.Caching.Memory;
+using Models;
 using Octokit;
 using Octokit.Internal;
 
@@ -12,47 +13,74 @@ internal class GitHubReleasesService(IMemoryCache memoryCache)
         new InMemoryCredentialStore(new Credentials(ApplicationConstants.GitHubReleaseToken))
     );
 
-    public async Task<Downloads> GetLatestStableDownloads()
+    public async Task<AppRelease> GetLatestStableDownloadsAsync()
     {
-        var releases = await GetReleases();
+        var releases = await GetReleasesAsync();
         if (releases.FirstOrDefault(x => x.Prerelease == false) is not { } latestRelease)
         {
             latestRelease = releases.FirstOrDefault();
             if (latestRelease is null)
             {
-                return DownloadsMissing.Instance;
+                return AppRelease.Missing;
             }
         }
 
-        if (await GetPortableDownloadAsset(latestRelease) is not { } portableAsset
-            || await GetInstallerDownloadAsset(latestRelease) is not { } installerAsset)
-        {
-            return DownloadsMissing.Instance;
-        }
+        return await CreateDownloadsAsync(latestRelease);
+    }
 
-        return new ReleaseDownloads
+    private async Task<AppRelease> CreateDownloadsAsync(Release latestRelease)
+        => new GitHubAppRelease
         {
             Release = latestRelease,
-            PortableAsset = portableAsset,
-            InstallerAsset = installerAsset,
+            PortableAsset = await CreatePortableAssetDownloadAsync(latestRelease),
+            InstallerAsset = await CreateInstallerAssetDownloadAsync(latestRelease),
+        };
+
+    private async Task<AssetDownload> CreatePortableAssetDownloadAsync(Release latestRelease)
+    {
+        if (await GetPortableDownloadAssetAsync(latestRelease) is not { } portableAsset)
+        {
+            return AssetDownload.Missing;
+        }
+
+        return new ReleaseAssetDownload
+        {
+            Name = portableAsset.Label,
+            Version = latestRelease.TagName,
+            BrowserDownloadUrl = portableAsset.BrowserDownloadUrl,
         };
     }
 
-    private async Task<ReleaseAsset?> GetPortableDownloadAsset(Release release)
+    private async Task<AssetDownload> CreateInstallerAssetDownloadAsync(Release latestRelease)
     {
-        var assets = await GetReleaseAssets(release);
+        if (await GetInstallerDownloadAssetAsync(latestRelease) is not { } installerAsset)
+        {
+            return AssetDownload.Missing;
+        }
+
+        return new ReleaseAssetDownload
+        {
+            Name = installerAsset.Label,
+            Version = latestRelease.TagName,
+            BrowserDownloadUrl = installerAsset.BrowserDownloadUrl,
+        };
+    }
+
+    private async Task<ReleaseAsset?> GetPortableDownloadAssetAsync(Release release)
+    {
+        var assets = await GetReleaseAssetsAsync(release);
         return assets.FirstOrDefault(asset => asset.Name.EndsWith("-Portable.zip", StringComparison.Ordinal));
     }
 
-    private async Task<ReleaseAsset?> GetInstallerDownloadAsset(Release release)
+    private async Task<ReleaseAsset?> GetInstallerDownloadAssetAsync(Release release)
     {
-        var assets = await GetReleaseAssets(release);
+        var assets = await GetReleaseAssetsAsync(release);
         return assets.FirstOrDefault(asset => asset.Name.EndsWith("-Setup.exe", StringComparison.Ordinal));
     }
 
-    private async Task<IReadOnlyList<ReleaseAsset>> GetReleaseAssets(Release release)
+    private async Task<IReadOnlyList<ReleaseAsset>> GetReleaseAssetsAsync(Release release)
         => await memoryCache.GetOrCreateAsync(
-               $"{nameof(GitHubReleasesService)}-{nameof(GetReleaseAssets)}",
+               $"{nameof(GitHubReleasesService)}-{nameof(GetReleaseAssetsAsync)}",
                async entry =>
                {
                    entry.SetSlidingExpiration(TimeSpan.FromHours(1));
@@ -61,9 +89,9 @@ internal class GitHubReleasesService(IMemoryCache memoryCache)
            )
            ?? [];
 
-    private async Task<IReadOnlyList<Release>> GetReleases()
+    private async Task<IReadOnlyList<Release>> GetReleasesAsync()
         => await memoryCache.GetOrCreateAsync(
-               $"{nameof(GitHubReleasesService)}-{nameof(GetLatestStableDownloads)}",
+               $"{nameof(GitHubReleasesService)}-{nameof(GetLatestStableDownloadsAsync)}",
                async entry =>
                {
                    entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
@@ -71,22 +99,4 @@ internal class GitHubReleasesService(IMemoryCache memoryCache)
                }
            )
            ?? [];
-}
-
-public class Downloads;
-
-public class DownloadsMissing : Downloads
-{
-    private DownloadsMissing()
-    {
-    }
-
-    public static Downloads Instance { get; } = new DownloadsMissing();
-}
-
-public class ReleaseDownloads : Downloads
-{
-    public required Release Release { get; init; }
-    public required ReleaseAsset PortableAsset { get; init; }
-    public required ReleaseAsset InstallerAsset { get; init; }
 }
