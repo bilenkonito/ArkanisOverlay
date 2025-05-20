6c80008048204ff458cdb5c7af4a1ef39755a663
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-05-20 08:28:24 +0200

    refactor: extract server host service registration

--- src/Arkanis.Overlay.Host.Server/Program.cs
+++ src/Arkanis.Overlay.Host.Server/Program.cs
@@ -1,18 +1,8 @@
 using System.Globalization;
-using Arkanis.Overlay.Common.Abstractions;
-using Arkanis.Overlay.Common.Enums;
-using Arkanis.Overlay.Common.Services;
-using Arkanis.Overlay.Components.Helpers;
-using Arkanis.Overlay.Components.Services;
+using Arkanis.Overlay.Host.Server;
 using Arkanis.Overlay.Host.Server.Components;
-using Arkanis.Overlay.Host.Server.Services;
-using Arkanis.Overlay.Infrastructure;
 using Arkanis.Overlay.Infrastructure.Data;
 using Arkanis.Overlay.Infrastructure.Data.Extensions;
-using Arkanis.Overlay.Infrastructure.Services;
-using Arkanis.Overlay.Infrastructure.Services.Abstractions;
-using MudBlazor;
-using MudBlazor.Services;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -21,39 +11,7 @@ Log.Logger = new LoggerConfiguration()
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSerilog(loggerConfig => loggerConfig
-    .Enrich.FromLogContext()
-    .ReadFrom.Configuration(builder.Configuration)
-);
-
-builder.Services.AddLogging();
-builder.Services.AddHttpClient();
-
-builder.Services.AddMemoryCache();
-
-builder.Services
-    .AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddMudServices(options =>
-    {
-        options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
-        options.SnackbarConfiguration.PreventDuplicates = false;
-        options.SnackbarConfiguration.ClearAfterNavigation = false;
-    }
-);
-
-builder.Services
-    .AddJavaScriptEventInterop()
-    .AddGlobalKeyboardProxyService()
-    .AddGoogleTrackingServices()
-    .AddSingleton<SharedAnalyticsPropertyProvider, ServerAnalyticsPropertyProvider>()
-    .AddServerOverlayControls()
-    .AddInfrastructure(options => options.HostingMode = HostingMode.Server)
-    .AddInfrastructureConfiguration(builder.Configuration)
-    .AddSingleton<GitHubReleasesService>()
-    .AddSingleton<IAppVersionProvider, AssemblyAppVersionProvider>()
-    .AddSingleton<ISystemAutoStartStateProvider, NoSystemAutoStartStateProvider>();
+builder.Services.AddAllServerHostServices(builder.Configuration);
 
 var app = builder.Build();
 
