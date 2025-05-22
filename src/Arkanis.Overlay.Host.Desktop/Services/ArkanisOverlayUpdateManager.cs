6df54435b60aabedbdd4531f280a009b0137e4f2
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-05-22 07:48:17 +0200

    chore: update velopack

--- src/Arkanis.Overlay.Host.Desktop/Services/ArkanisOverlayUpdateManager.cs
+++ src/Arkanis.Overlay.Host.Desktop/Services/ArkanisOverlayUpdateManager.cs
@@ -1,16 +1,14 @@
 namespace Arkanis.Overlay.Host.Desktop.Services;
 
-using Microsoft.Extensions.Logging;
 using Velopack;
 using Velopack.Locators;
 using Velopack.Sources;
 
 public class ArkanisOverlayUpdateManager(
     IUpdateSource source,
-    ILogger<ArkanisOverlayUpdateManager> logger,
     UpdateOptions? options = null,
     IVelopackLocator? locator = null
-) : UpdateManager(source, options, logger, locator)
+) : UpdateManager(source, options, locator)
 {
     public string CurrentChannel
         => Channel;
