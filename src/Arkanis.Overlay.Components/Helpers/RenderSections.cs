3f694f8f55e688d6dfcf779947facb16d395238b
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-06-03 09:21:57 +0200

    refactor: extract overlay controls to shared layout

--- src/Arkanis.Overlay.Components/Helpers/RenderSections.cs
+++ src/Arkanis.Overlay.Components/Helpers/RenderSections.cs
@@ -7,4 +7,12 @@ public static class RenderSections
         public const string AppMenu = $"{nameof(AppBar)}-{nameof(AppMenu)}";
         public const string UserMenu = $"{nameof(AppBar)}-{nameof(UserMenu)}";
     }
+
+    public static class OverlayControls
+    {
+        public const string TopRight = $"{nameof(OverlayControls)}-{nameof(TopRight)}";
+        public const string BottomRight = $"{nameof(OverlayControls)}-{nameof(BottomRight)}";
+        public const string TopLeft = $"{nameof(OverlayControls)}-{nameof(TopLeft)}";
+        public const string BottomLeft = $"{nameof(OverlayControls)}-{nameof(BottomLeft)}";
+    }
 }
