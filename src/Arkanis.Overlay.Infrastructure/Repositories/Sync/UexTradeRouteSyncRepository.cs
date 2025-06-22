616ad5bb2b14df3ea6a768b3dbeedc131f81ee4c
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-06-22 08:11:40 +0200

    fix: remove duplicities from external data (trade routes)

--- src/Arkanis.Overlay.Infrastructure/Repositories/Sync/UexTradeRouteSyncRepository.cs
+++ src/Arkanis.Overlay.Infrastructure/Repositories/Sync/UexTradeRouteSyncRepository.cs
@@ -56,6 +56,12 @@ internal class UexTradeRouteSyncRepository(
     protected override bool IncludeSourceModel(CommodityRouteDTO sourceModel)
         => sourceModel is { Price_origin: > 0, Price_destination: > 0, Id_terminal_origin: > 0, Id_terminal_destination: > 0 };
 
+    protected override IEnumerable<CommodityRouteDTO> FilterSourceModels(IEnumerable<CommodityRouteDTO> models)
+        => models
+            .OrderByDescending(x => x.Date_added)
+            .GroupBy(x => (x.Id_commodity, x.Id_terminal_origin, x.Id_terminal_destination))
+            .Select(x => x.First());
+
     protected override UexApiGameEntityId? GetSourceApiId(CommodityRouteDTO source)
         => source.Id is not null
             ? Mapper.CreateGameEntityId(source, x => x.Id)
